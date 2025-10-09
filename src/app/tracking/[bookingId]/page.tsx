81cb465b55d17d21f8004d8cf1d77932517730c8
Faraz Khoso <farazkhoso299@gmail.com>
2025-10-09 11:29:34 +0000

    Build Error
    Parsing ecmascript source code failed
    
    ./src/app/tracking/

--- src/app/tracking/[bookingId]/page.tsx
+++ src/app/tracking/[bookingId]/page.tsx
@@ -196,7 +196,7 @@ export default function TrackingPage() {
                         ))}
                     </CardContent>
                 </Card>
-            )
+            );
         }
         return null;
     }
